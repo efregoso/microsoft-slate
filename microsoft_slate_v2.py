d2e76f12f53f61be8f6b38c8ed78dd09b62b047c
Elizabeth Fregoso <emf65@case.edu>
2018-11-23 21:54:01 -0500

    Updated main program & included result from first few lines of beemovie script.  Enjoy.

--- microsoft_slate_v2.py
+++ microsoft_slate_v2.py
@@ -26,45 +26,59 @@ import sys
 def main(argv):
 	# Examine all text files and select the one with the same name as the designated filetype.
 	filename = argv[1]
+	# DEBUG: DO MORE LOOKING INTO THIS RE: ONLY GATHERING FILES *ONLY* FROM CWD
 	for root, dirs, files in os.walk(os.getcwd()):
-		if filename not in files:
-			print("Filename not found!")
-			print(files)
-			sys.exit(1)
-		else:
-			#try: 
-			orig = open(filename, "r")
-			if filename + "_BEES_indicator.txt" in files:
-				new = open(filename + "_BEES_indicator.txt", "w")
+		if root == os.getcwd():
+			if filename not in files:
+				print("Filename not found!")
+				print(root)
+				print(files)
+				sys.exit(1)
 			else:
-				new = open(filename + "_BEES_indicator.txt", "a")
-			buffer = orig.readline()
-			while buffer is not "":
-				if buffer is not "\n" and not buffer.isspace():
-					newstring = buffer.rstrip("\n")
-					newstring_indicators = ""
-					# ERROR HANDLING FOR COMMAS, SPACES, NUMBERS, AND SPECIAL CHARACTERS:
-					for letter in newstring:
-						if letter.isalpha():
-							letter = letter.lower()
-							letter_indicator = ":regional_indicator_" + letter + ":"
-						elif letter.isnumeric():
-							# Dictionary object with spelled out reference. 
-							dict = {"0":"zero", "1": "one", "2":"two", "3":"three", "4":"four", "5":"five", "6":"six", "7":"seven", "8":"eight", "9":"nine"}
-							letter_indicator = ":" + dict[letter] + ":"
-						elif letter.isspace():
-							letter_indicator = " "
-						else:
-							letter_indicator = ""
-						newstring_indicators = newstring_indicators + letter_indicator + " "
-					newstring_indicators = newstring_indicators + "\n"
-					new.write(newstring_indicators)
+				#try:
+				# Check if the filename entered by the user does not have the txt suffix, and add it if not. 
+				if not filename.endswith(".txt"):
+					filename = filename + ".txt"
+				# Open the filename, with ".txt" suffix.
+				orig = open(filename, "r")
+				# Remove the txt suffix again. 
+				filename = filename.rstrip(".txt")
+				# If an indicator file already exists, write a new one.  If not, make a new one.
+				if filename + "_BEES_indicator.txt" in files:
+					new = open(filename + "_BEES_indicator.txt", "w")
+				else:
+					new = open(filename + "_BEES_indicator.txt", "a")
+				# Read a single line from the parent file. 
 				buffer = orig.readline()
-				
-			# except Exception:
-				# print("Read/write file failed.  Check that you have permission to write to this directly.")
-				# sys.exit(1)
-	print("'Well, thank Heavens it's over... hopefully that never happens again.'")
+				# While the buffer is not EOF, format the line into Optimal Awkward Format (OAF).
+				while buffer is not "":
+					if buffer is not "\n" and not buffer.isspace():
+						newstring = buffer.rstrip("\n")
+						newstring_indicators = ""
+						# ERROR HANDLING FOR COMMAS, SPACES, NUMBERS, AND SPECIAL CHARACTERS:
+						for letter in newstring:
+							if letter.isalpha():
+								letter = letter.lower()
+								letter_indicator = ":regional_indicator_" + letter + ":"
+							elif letter.isnumeric():
+								# Dictionary object with spelled out reference. 
+								dict = {"0":"zero", "1": "one", "2":"two", "3":"three", "4":"four", "5":"five", "6":"six", "7":"seven", "8":"eight", "9":"nine"}
+								letter_indicator = ":" + dict[letter] + ":"
+							elif letter.isspace():
+								letter_indicator = " "
+							else:
+								letter_indicator = ""
+							newstring_indicators = newstring_indicators + letter_indicator + " "
+						newstring_indicators = newstring_indicators + "\n"
+						new.write(newstring_indicators)
+					buffer = orig.readline()
+					
+				# except Exception:
+					# print("Read/write file failed.  Check that you have permission to write to this directly.")
+					# sys.exit(1)
+	# Wake up from a dead faint. 
+	print("'Where am I...?  WHAT HAPPENED...?!'")
+	# Curtain. 
 	sys.exit(0)
 
 
